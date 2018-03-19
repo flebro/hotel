490f446be2f5a60af373a65dbfee4013d8c8eb5f
Floxaga <florian.victoriadt@gmail.com>
2018-03-19 11:37:06 +0100

    Ouverture avec l'ip et non plus en local

--- src/com/iia/webservices/groupa/hotel/ApplicationConfig.java
+++ src/com/iia/webservices/groupa/hotel/ApplicationConfig.java
@@ -17,7 +17,8 @@ public class ApplicationConfig extends Application {
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("1.0.2");
         beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:8080");
+        /*beanConfig.setHost("localhost:8080");*/
+        beanConfig.setHost("192.168.43.191:8080");
         beanConfig.setBasePath("/hotel/rest");
         beanConfig.setResourcePackage("com.iia.webservices.groupa.hotel");
         beanConfig.setScan(true);
