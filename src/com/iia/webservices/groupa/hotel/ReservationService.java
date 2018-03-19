acd68472052646729bac3583d725f827b1327a04
Fabien Le Bronnec <fabien.lebronnec@compta.com>
2018-03-19 11:36:50 +0100

    Correction find hotel

--- src/com/iia/webservices/groupa/hotel/ReservationService.java
+++ src/com/iia/webservices/groupa/hotel/ReservationService.java
@@ -35,24 +35,11 @@ public class ReservationService {
 	@Path("/")
 	@Consumes("application/json")
 	@Produces("application/json")
-<<<<<<< HEAD
-	/**
-	 * Permet de réserver un hôtel
-	 * @param reservationDemande est un objet de réservation allégée contenant deux dates en string (AAAA-MM-JJ) et un id d'hôtel (Integer)
-	 * @return Code Réponse OK 
-	 */
-	public Response ReservationHotel(ReservationDemande reservationDemande){
-		// on test chaque paramètre avant de construire notre résa
-		if(reservationDemande.getDateDebDemande() == null || 
-				reservationDemande.getDateFinDemande() == null || 
-				reservationDemande.getHotelIDDemande() == null) {
-=======
 	public Response ReservationHotel(ReservationDO reservationDemande){
 		// on test chaque paramï¿½tre avant de construire notre rï¿½sa
 		if(reservationDemande.getDateDebut() == null || 
 				reservationDemande.getDateFin() == null || 
 				reservationDemande.getHotel() == null) {
->>>>>>> branch 'master' of https://github.com/flebro/hotel
 			Response.status(HttpResponseCodes.SC_BAD_REQUEST).entity("You must provide all parameters").build();
 		}
 		
@@ -85,10 +72,10 @@ public class ReservationService {
 	@Path("/")
 	@Produces("application/json")
 	/**
-	 * Permet de lister les réservations , les paramètres sont facultatifs. Si ils ne sont pas renseignés, retourne l'intégralité des réservations.
-	 * @param hotelId Id de l'hôtel de type Integer
+	 * Permet de lister les rï¿½servations , les paramï¿½tres sont facultatifs. Si ils ne sont pas renseignï¿½s, retourne l'intï¿½gralitï¿½ des rï¿½servations.
+	 * @param hotelId Id de l'hï¿½tel de type Integer
 	 * @param dateStr Date de vision en string au format AAAA-MM-JJ
-	 * @return une liste de réservation correspondant aux paramètres spécifiés.Si ils ne sont pas renseignés, retourne l'intégralité des réservations.
+	 * @return une liste de rï¿½servation correspondant aux paramï¿½tres spï¿½cifiï¿½s.Si ils ne sont pas renseignï¿½s, retourne l'intï¿½gralitï¿½ des rï¿½servations.
 	 */
 	public Response listeReservation(@QueryParam("hotelId") Integer hotelId, @QueryParam("date") String dateStr ){
 		LocalDate date = null;
